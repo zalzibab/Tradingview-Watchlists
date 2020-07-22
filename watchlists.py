f7e926653d8a95b41fbd8b2e8ae5aa597cd137db
zalzibab <zalzibab@github.com>
2020-07-22 15:54:31 -0400

    Update Exchanges
    added functionality for Bitfinex, Bittrex, CoinbasePro, HitBTC, Huobi, and OKEx

--- watchlists.py
+++ watchlists.py
@@ -30,3 +30,38 @@ BINANCE = ['BINANCE:'+x['symbol'] for x in requests.get('https://api.binance.com
 BINANCE.sort()
 write_txt('BINANCE.txt', BINANCE)
 
+
+BITFINEX = []
+BITFINEX_raw = requests.get('https://api-pub.bitfinex.com/v2/tickers?symbols=ALL').json()
+for ticker in range(len(BITFINEX_raw)):
+    BITFINEX.append(BITFINEX_raw[ticker][0])
+BITFINEX = ['BITFINEX:'+x[1:] for x in BITFINEX if x[0] == 't' and ':' not in x and 'CNHT' not in x and 'TEST' not in x and 'BBB' not in x]
+BITFINEX.sort()
+write_txt('BITFINEX.txt', BITFINEX)
+
+
+HUOBI = ['HUOBI:'+x['symbol'].upper() for x in requests.get('https://api.huobi.pro/market/tickers').json()['data'] if x['symbol'].upper()[-2:] != 'HT' and '10' not in x['symbol'].upper() and 'BTC3' not in x['symbol'].upper() and 'X8' not in x['symbol'].upper() and 'BTC1' not in x['symbol'].upper()]
+HUOBI.sort()
+write_txt('HUOBI.txt', HUOBI)
+
+
+HITBTC = ['HITBTC:'+x['id'] for x in requests.get('https://api.hitbtc.com/api/2/public/symbol').json()]
+HITBTC.sort()
+write_txt('HITBTC.txt', HITBTC)
+
+
+BITTREX = ['BITTREX:'+x['symbol'].replace('-', '') for x in requests.get('https://api.bittrex.com/v3/markets/tickers').json()]
+BITTREX.sort()
+write_txt('BITTREX.txt', BITTREX)
+
+
+COINBASE = ['COINBASE:'+x['id'].replace('-', '') for x in requests.get('https://api.pro.coinbase.com/products').json()]
+COINBASE.sort()
+write_txt('COINBASE.txt', COINBASE)
+
+
+OKEX_raw = [x['instrument_id'].replace('-', '')[:-4] for x in requests.get('https://www.okex.com/api/swap/v3/instruments/ticker').json()]
+OKEX = ['OKEX:'+x for x in OKEX_raw if x[-1] == 'T'] + ['OKEX:BTCUSDTPERP', 'OKEX:BTCUSDPERP', 'OKEX:BTCUSDT3M', 'OKEX:BTCUSD3M', 'OKEX:BTCUSDT6M', 'OKEX:BTCUSD6M', 'OKEX:BTCUSDT1W', 'OKEX:BTCUSD1W', 'OKEX:BTCUSDT2W', 'OKEX:BTCUSD2W']
+OKEX.sort()
+write_txt('OKEX.txt', OKEX)
+
